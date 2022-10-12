f41777e060efbc9d5a8b87f544a2444338b67056
Bukola Eunice <thelmabukky87@gmail.com>
2022-10-12 19:10:46 +0100

    second screen done

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -4,4 +4,7 @@ const kBackground = Color(0xff201F1F);
 const kLinearOrange = Color(0xff53342B);
 const kLightOrange = Color(0xffFFCEB5);
 const kLightText = Color(0xff9B9B9B);
+const kOrange = Color(0xffFF7955);
+const kContainer = Color(0xff262626);
+const kBigText = Color(0xffFAFAFA);
 // 4A3029
